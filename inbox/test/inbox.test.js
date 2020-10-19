b934734109d6e00eb3d74e4f06fc5459335bd719
jean riffel <jeanriffel1@gmail.com>
2020-10-19 20:37:19 -0300

    updating source of test

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -6,6 +6,7 @@ const {interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+let INITIAL_MESSAGE = 'Hi there';
 
 beforeEach(async ()=>{
 
@@ -14,13 +15,23 @@ beforeEach(async ()=>{
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({data : bytecode, arguments : ['Hi there']})
         .send({ from : accounts[0], gas:'1000000'});
-
 });
 
 describe('Inbox', ()=>{
     it ('deploys contracts', ()=>{
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
 
+
+    it ('has a default message', async ()=>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('can change the message', async ()=>{
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');        
     });
 
 });
